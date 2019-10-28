ba3f657387ef55b844cff38ced13c1401926fad6
tomsirat <sirat.tom@outlook.com>
2019-10-28 10:23:47 +0100

    a toi de jouer Sacha

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $("#reset").click(function(){
+        console.log(($("#exampleFormControlTextarea1").val().length))        
+    })
+
+    $("#formulaire").on("submit" , function() {
+
+        // if ($("#exampleInputPassword1").val() != $("#exampleInputPassword2").val()) {
+
+        // }
+
+        // if ($("#exampleInputEmail1").val().length < 5 || ($("exampleInputPassword1").val().length < 5) || ($("exampleInputPassword1").val().length < 5) || ($("exampleFormControlTextarea1").val().length < 5)) {
+        //     console.log("ok");
+            
+        // }
+
+        //  console.log(($("#exampleFormControlTextarea1").val().length))
+         
+        // return false 
+
+    });
+    
+   
+});
